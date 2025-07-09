97fc0bb849bacd85fca2b6fd114bec1d186d7906
Pixel <souptik0537@gmail.com>
2025-07-09 13:42:04 +0530

    adjustment3

--- src/main/java/com/booking/application/controller/AuthController.java
+++ src/main/java/com/booking/application/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -81,6 +80,7 @@ public class AuthController {
 
                 if (user.getRoles().contains(Role.USER)) {
                         if (!user.getRoles().contains(Role.HOST)) {
+                                user.getRoles().clear();
                                 user.getRoles().add(Role.HOST);
                                 user_repo.save(user);
                         }
